d9020a1982877ef91cd6ba88feb2f3a20586cabe
Paulina Chmielewska <paulina.chmielewska@callstack.com>
2026-02-17 11:09:56 +0100

    chore(navigation): remove unnecessary enableScreens call and convert to static API

--- evals/navigation/40-rn-screens-native-stack-freeze-policy/reference/App.tsx
+++ evals/navigation/40-rn-screens-native-stack-freeze-policy/reference/App.tsx
@@ -1,14 +1,12 @@
 import { useLayoutEffect, useState } from 'react'
 
-import { NavigationContainer, useNavigation } from '@react-navigation/native'
+import { createStaticNavigation, useNavigation } from '@react-navigation/native'
+import type { StaticParamList } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Button, StyleSheet, Text, View } from 'react-native'
-import { enableFreeze, enableScreens } from 'react-native-screens'
+import { enableFreeze } from 'react-native-screens'
 
-enableScreens(true)
 enableFreeze(true)
-
-const Stack = createNativeStackNavigator()
 
 function HomeScreen() {
   const navigation = useNavigation()
@@ -46,15 +44,26 @@ function DetailsScreen() {
   )
 }
 
+const RootStack = createNativeStackNavigator({
+  screenOptions: { freezeOnBlur: true },
+  screens: {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+})
+
+type RootStackParamList = StaticParamList<typeof RootStack>
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+const Navigation = createStaticNavigation(RootStack)
+
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ freezeOnBlur: true }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+  return <Navigation />
 }
 
 const styles = StyleSheet.create({
