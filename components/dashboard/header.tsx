c14cbc6316c37111e9d1439378064f126bfa74fe
Damien <damian.redecki@gmail.com>
2026-03-05 19:27:38 +0100

    Update header.tsx

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -5,7 +5,7 @@ import { CallstackLogo } from "@/components/dashboard/callstack-logo";
 import { NavigationCategories, type DashboardTabId } from "@/components/dashboard/nav-categories";
 import type { CategoryDefinition } from "@/lib/types/evals";
 
-const RESULTS_REPO_URL = "https://github.com/callstack";
+const RESULTS_REPO_URL = "https://github.com/callstackincubator/evals";
 
 interface DashboardHeaderProps {
   categories: CategoryDefinition[];
